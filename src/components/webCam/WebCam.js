e9fdeb29011fd4baab1807ea849249d43c144f88
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-23 11:05:14 +0600

    webcam

---
+++ src/components/webCam/WebCam.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Webcam from "react-webcam";
+
+
+const WebCam = () => {
+    const webcamRef = React.useRef(null);
+  const mediaRecorderRef = React.useRef(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm"
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+    return (
+        <div>
+            <h1 className='text-5xl text-center text-red-400 font-bold'>Web cam</h1>
+
+   <div className='flex justify-center mt-5'>
+   <Webcam audio={false} ref={webcamRef} className=' rounded-xl' />
+   </div>
+   <div className='mt-5  text-center'>
+     {capturing ? (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button className="bg-yellow-700 hover:bg-gray-500 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleDownload} className="bg-red-600 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Download</button>
+      )}
+     </div>
+        </div>
+    );
+};
+
+export default WebCam;
